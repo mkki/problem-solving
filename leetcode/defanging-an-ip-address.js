4fe385dad5ce67b8eadf8c544e57b64ffac08958
mkki <devmkki@gmail.com>
2021-04-18 19:07:07 +0900

    Fix syntax

--- leetcode/defanging-an-ip-address.js
+++ leetcode/defanging-an-ip-address.js
@@ -2,6 +2,6 @@
  * @param {string} address
  * @return {string}
  */
- var defangIPaddr = function(address) {
+var defangIPaddr = function(address) {
     return address.split(".").join("[.]");
 };
--- leetcode/running-sum-of-1d-array.js
+++ leetcode/running-sum-of-1d-array.js
@@ -2,15 +2,15 @@
  * @param {number[]} nums
  * @return {number[]}
  */
- var runningSum = function(nums) {
+var runningSum = function(nums) {
     let result = [];
-    
+
     nums.reduce((accumulator, current) => {
         accumulator += current;
         result.push(accumulator);
-        
+
         return accumulator;
     }, 0);
-    
+
     return result;
 };
