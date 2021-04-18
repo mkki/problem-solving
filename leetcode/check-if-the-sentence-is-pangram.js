86a867f9e56862b1e75bb5f8707c452f2c408717
mkki <devmkki@gmail.com>
2021-04-18 18:12:39 +0900

    Add solution for #1832

---
+++ leetcode/check-if-the-sentence-is-pangram.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+ var checkIfPangram = function(sentence) {
+    let checksums = new Array(26);
+    const splitedSentence = sentence.split("");
+    
+    for(const element of splitedSentence) {
+        const index = element.charCodeAt(0) - 97;
+        checksums[index] = true;
+    }
+    
+    for(const element of checksums) {
+        if (!element) {
+            return false;
+        }
+    }
+    
+    return true;
+};
