9c72d5f9c638fa4922953465d8d13fc0355a21eb
mkki <devmkki@gmail.com>
2021-04-18 19:06:42 +0900

    Add solution for #1672

---
+++ leetcode/richest-customer-wealth.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let maxValue = 0;
+
+    for (const account of accounts) {
+        let totalAmount = 0;
+
+        for (const amount of account) {
+            totalAmount += amount;
+        }
+
+        maxValue = Math.max(maxValue, totalAmount);
+    }
+
+    return maxValue;
+};
