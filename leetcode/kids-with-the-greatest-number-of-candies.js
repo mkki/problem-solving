f3d08c3b565f45cc065c5ef61bd283e5faf2b487
mkki <devmkki@gmail.com>
2021-04-18 19:25:05 +0900

    Add solution for #1431

---
+++ leetcode/kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let result = [];
+    let maximumNumberOfCandies = 0;
+
+    for (const candy of candies) {
+        maximumNumberOfCandies = Math.max(maximumNumberOfCandies, candy);
+    }
+
+    for (const candy of candies) {
+        const modifiedNumberOfCandies = candy + extraCandies;
+
+        if (modifiedNumberOfCandies >= maximumNumberOfCandies) {
+            result.push(true);
+        } else {
+            result.push(false);
+        }
+    }
+
+    return result;
+};
