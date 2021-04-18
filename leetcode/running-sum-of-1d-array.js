5bf3ef0a63ef12829861dabf60b5fa8ab0d799bc
mkki <devmkki@gmail.com>
2021-04-18 18:41:45 +0900

    Add solution for #1480

---
+++ leetcode/running-sum-of-1d-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var runningSum = function(nums) {
+    let result = [];
+    
+    nums.reduce((accumulator, current) => {
+        accumulator += current;
+        result.push(accumulator);
+        
+        return accumulator;
+    }, 0);
+    
+    return result;
+};
