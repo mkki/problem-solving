f6ab4c9c35bf43d570057a940386e77ee3ac9e0c
mkki <devmkki@gmail.com>
2021-04-25 19:02:36 +0900

    Add solution for #1470

---
+++ leetcode/shuffle-the-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    const result = [];
+    const firstArray = nums.slice(0, n + 1);
+    const secondArray = nums.slice(n, 2 * n + 1);
+
+    for (let i = 0; i < n; i++) {
+        result.push(firstArray[i]);
+        result.push(secondArray[i]);
+    }
+
+    return result;
+};
