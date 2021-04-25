73e6d4c9e82066c68d4061da7cc3bbbf9fb00aca
mkki <devmkki@gmail.com>
2021-04-25 20:02:05 +0900

    Add solution for #1365

---
+++ leetcode/how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+    return nums.reduce((accumulator, current) => {
+        let smallerNumbersCount = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] < current) smallerNumbersCount++;
+        }
+
+        return [...accumulator, smallerNumbersCount];
+    }, []);
+};
