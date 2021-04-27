9d4ef9d89e1b4474a28fd53884a978676707b91c
mkki <devmkki@gmail.com>
2021-04-27 15:28:39 +0900

    Add solution for #1281

---
+++ leetcode/subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    return [...n + ""].reduce((acc, el) => acc *= parseInt(el), 1) -
+        [...n + ""].reduce((acc, el) => acc += parseInt(el), 0);
+};
