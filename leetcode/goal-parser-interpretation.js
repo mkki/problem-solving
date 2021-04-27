e9d8ef7100e5facfc71d61020675127b5a0c9447
mkki <devmkki@gmail.com>
2021-04-27 19:20:53 +0900

    Add solution for #1678

---
+++ leetcode/goal-parser-interpretation.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function(command) {
+    let result = "";
+    let isParentheses = false;
+
+    [...command].forEach(element => {
+        if (element === '(') {
+            isParentheses = true;
+            return;
+        }
+
+        if (element === ')') {
+            if (isParentheses) {
+                result += 'o';
+                isParentheses = false;
+            }
+
+            return;
+        }
+
+        result += element;
+        isParentheses = false;
+    });
+
+    return result;
+};
