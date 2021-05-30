fd0e6ec21d4f347d87d6376a122f7c5b37d08836
mkki <devmkki@gmail.com>
2021-05-30 14:19:19 +0900

    Add solution for #1773

---
+++ leetcode/count-items-matching-a-rule.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function(items, ruleKey, ruleValue) {
+    let result = 0;
+    let index;
+
+    switch (ruleKey) {
+        case "type":
+            index = 0;
+            break;
+        case "color":
+            index = 1;
+            break;
+        default:
+            index = 2;
+            break;
+    }
+
+    for (item of items) {
+        if (item[index] === ruleValue) {
+            result += 1;
+        }
+    }
+
+    return result;
+};
