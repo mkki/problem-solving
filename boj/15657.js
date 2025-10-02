6f7c03978618d93a6389541e818aa3050e02cf67
mkki <devmkki@gmail.com>
2025-10-03 01:10:50 +0900

    Add solution for boj #15657

---
+++ boj/15657.js
@@ -0,0 +1,27 @@
+const [n, m, ...numbers] = require('fs')
+  .readFileSync(0, 'utf-8')
+  .trim()
+  .split('\n')
+  .flatMap((v) => v.split(' ').map(Number));
+
+const currentNumbers = [];
+const result = [];
+
+numbers.sort((a, b) => a - b);
+
+const recursive = (index, count) => {
+  if (count === m) {
+    result.push(currentNumbers.join(' '));
+    return;
+  }
+
+  for (let i = index; i < n; i++) {
+    const number = numbers[i];
+    currentNumbers[count] = number;
+    recursive(i, count + 1);
+  }
+};
+
+recursive(0, 0);
+
+console.log(result.join('\n'));
